d5b1588e9620f8af9579d251e95056da4f28a3c9
Ivan <45792439+lyskoivan@users.noreply.github.com>
2019-11-17 00:51:38 +0200

    Update index.js

--- index.js
+++ index.js
@@ -2,10 +2,3 @@ const { port } = require("./config");
 const startServer = require("./src/server");
 
 startServer(port);
-
-/*
-1. added get for id
-2. added get with many id (map + qs)
-3. added get for category (filter)
- 3.1 if (!prdoucts) status = no prod
-*/
